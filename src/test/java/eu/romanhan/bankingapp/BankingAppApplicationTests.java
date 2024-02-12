e0b9938cc745e3a4f2999856d293afb4e5866ee4
Roman.Hanmamedov <Roman.Hanmamedov@EST-111.EST.local>
2024-02-12 15:46:39 +0200

    Initial commit.

---
+++ src/test/java/eu/romanhan/bankingapp/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.romanhan.bankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
